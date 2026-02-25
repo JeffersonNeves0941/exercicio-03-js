5ac9ac1862b5c987797824ed6a162f70ba520b3e
JeffersonNeves0941 <25204074@cfp127aluno.local>
2026-02-25 17:01:25 -0300

    upando para o git

---
+++ app.js
@@ -0,0 +1,1 @@
+const calculoMatematico = require("./modulo/calcular.js")
---
+++ modulo/calcular.js
@@ -0,0 +1,8 @@
+const calcularImc = function(altura, peso){
+    valorPeso = Number(peso)
+    valorAltura = Number(altura
+    
+    calculoImc
+    )
+
+}
